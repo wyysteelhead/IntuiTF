25cb5893a9426c087887ea8378fa86b344c7bf02
wyysteelhead <wangyiyao@zju.edu.cn>
2025-08-11 19:31:11 +0800

    removed unused

--- diffdvr/__init__.py
+++ diffdvr/__init__.py
@@ -2,7 +2,6 @@ from .utils import make_real3, make_real4, \
   inverseSigmoid, InverseSigmoid, \
   inverseSoftplus, InverseSoftplus, \
   implies, toCHW, fibonacci_sphere, \
-  renderer_dtype_torch, renderer_dtype_np, \
   cvector_to_numpy
 
 
